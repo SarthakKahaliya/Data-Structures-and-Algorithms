29eff1f71647f1ad5a6628698bc25e097a27828d
SarthakKahaliya <64351234+SarthakKahaliya@users.noreply.github.com>
2022-04-21 20:10:14 -0400

    Time: 657 ms (63.21%), Space: 54.9 MB (22.95%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -8,23 +8,34 @@ class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         
         
-        if not root:
+#         if not root:
+#             return 0
+        
+#         self.min_depth = float("inf")
+        
+#         def dfs(root, depth):
+#             if not root:
+#                 return
+                
+#             if not root.left and not root.right:
+#                 self.min_depth = min(self.min_depth, depth)
+#             else:
+#                 if root.left:
+#                     dfs(root.left, depth+1)
+#                 if root.right:
+#                     dfs(root.right, depth+1)
+                
+#         dfs(root, 1)
+        
+#         return self.min_depth
+
+        if root is None:
             return 0
-        
-        self.min_depth = float("inf")
-        
-        def dfs(root, depth):
-            if not root:
-                return
-                
-            if not root.left and not root.right:
-                self.min_depth = min(self.min_depth, depth)
-            else:
-                if root.left:
-                    dfs(root.left, depth+1)
-                if root.right:
-                    dfs(root.right, depth+1)
-                
-        dfs(root, 1)
-        
-        return self.min_depth
+        left_depth = self.minDepth(root.left)
+        right_depth = self.minDepth(root.right)
+        if left_depth == 0:
+            return right_depth + 1
+        elif right_depth == 0:
+            return left_depth + 1
+        else:
+            return min(left_depth, right_depth) + 1
