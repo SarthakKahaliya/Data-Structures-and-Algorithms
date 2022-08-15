a71d8ef8d21893ffcfa8cd6ab4db90508711a63f
SarthakKahaliya <64351234+SarthakKahaliya@users.noreply.github.com>
2022-08-15 02:22:06 -0400

    Time: 500 ms (73.73%), Space: 19.5 MB (5.22%) - LeetHub

--- 170-two-sum-iii-data-structure-design/170-two-sum-iii-data-structure-design.py
+++ 170-two-sum-iii-data-structure-design/170-two-sum-iii-data-structure-design.py
@@ -2,21 +2,21 @@ class TwoSum:
 
     def __init__(self):
         self.array = []
+        self.hashMap = {}
         
 
     def add(self, number: int) -> None:
         self.array.append(number)
+        self.hashMap[number] = len(self.array) - 1
         
 
     def find(self, value: int) -> bool:
-        hashMap = {}
         for i, val in enumerate(self.array):
             diff = value - val
             
-            if diff in hashMap:
+            if diff in self.hashMap and self.hashMap[diff] != i:
+                
                 return True
-            else:
-                hashMap[val] = True
             
         
         return False
