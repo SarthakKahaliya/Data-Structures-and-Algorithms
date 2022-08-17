cbc6d46c2372575ff7748ef1d60c039de5f213b7
SarthakKahaliya <64351234+SarthakKahaliya@users.noreply.github.com>
2022-08-17 02:39:48 -0400

    Time: 238 ms (46.59%), Space: 18.1 MB (85.23%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -2,16 +2,28 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
         intervals.sort()
-        newIntervals = [intervals[0]]
+        # newIntervals = [intervals[0]]
         start = 0
         last = -1
         
-        for i in range(1, len(intervals)):
-            interval = intervals[i]
-            lastNewInterval = newIntervals[last]
-            if lastNewInterval[last] >= interval[start]:
-                newIntervals[last] = [lastNewInterval[start],max(lastNewInterval[last], interval[last])]
+        # for i in range(1, len(intervals)):
+        #     interval = intervals[i]
+        #     lastNewInterval = newIntervals[last]
+        #     if lastNewInterval[last] >= interval[start]:
+        #         newIntervals[last] = [lastNewInterval[start],max(lastNewInterval[last], interval[last])]
+        #     else:
+        #         newIntervals.append(interval)
+        
+        # return newIntervals
+        
+        i = 1
+        while i < len(intervals):
+            currInterval = intervals[i]
+            lastInterval = intervals[i-1]
+            if lastInterval[last] >= currInterval[start]:
+                intervals[i] = [lastInterval[start], max(lastInterval[last], currInterval[last])]
+                intervals.pop(i-1)
             else:
-                newIntervals.append(interval)
+                i += 1
                 
-        return newIntervals
+        return intervals
