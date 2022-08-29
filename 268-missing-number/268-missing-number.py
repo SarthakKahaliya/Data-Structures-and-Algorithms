7e23c12857a95785b4a6a7fb31dc6340161628ff
SarthakKahaliya <64351234+SarthakKahaliya@users.noreply.github.com>
2022-08-29 15:21:17 -0400

    Time: 253 ms (35.26%), Space: 15.2 MB (79.00%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,11 +1,20 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        array = [None for i in range(len(nums) + 1)]
+#         array = [None for i in range(len(nums) + 1)]
         
-        for val in nums:
-            array[val] = val
+#         for val in nums:
+#             array[val] = val
             
         
-        for i in range(len(array)):
-            if array[i] == None:
-                return i
+#         for i in range(len(array)):
+#             if array[i] == None:
+#                 return i
+        
+        # highest no. is 
+        lastNum = len(nums)
+        
+        for i, num in enumerate(nums):
+            lastNum ^= i ^ num
+            
+        return lastNum
+        
